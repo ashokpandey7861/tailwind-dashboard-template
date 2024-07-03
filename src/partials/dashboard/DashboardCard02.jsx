5b339f5f242a7f80ca62a7a157e5d790a643cc4d
ashokpandey7861 <94458715+ashokpandey7861@users.noreply.github.com>
2024-07-03 11:15:41 +0530

    Update DashboardCard02.jsx

--- src/partials/dashboard/DashboardCard02.jsx
+++ src/partials/dashboard/DashboardCard02.jsx
@@ -68,28 +68,7 @@ function DashboardCard02() {
   return (
     <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-2 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
       <div className="px-5 pt-5">
-        <header className="flex justify-between items-start mb-2">
-          {/* Icon */}
-          <img src={Icon} width="32" height="32" alt="Icon 02" />
-          {/* Menu button */}
-          <EditMenu align="right" className="relative inline-flex">
-            <li>
-              <Link className="font-medium text-sm text-slate-600 dark:text-slate-300 hover:text-slate-800 dark:hover:text-slate-200 flex py-1 px-3" to="#0">
-                Option 1
-              </Link>
-            </li>
-            <li>
-              <Link className="font-medium text-sm text-slate-600 dark:text-slate-300 hover:text-slate-800 dark:hover:text-slate-200 flex py-1 px-3" to="#0">
-                Option 2
-              </Link>
-            </li>
-            <li>
-              <Link className="font-medium text-sm text-rose-500 hover:text-rose-600 flex py-1 px-3" to="#0">
-                Remove
-              </Link>
-            </li>
-          </EditMenu>
-        </header>
+        
         <h2 className="text-lg font-semibold text-slate-800 dark:text-slate-100 mb-2">Bank Nifty</h2>
 
         <div className="flex items-start">
