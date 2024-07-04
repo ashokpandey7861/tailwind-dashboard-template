fdcf56b8a9a31d4a9f9285e422e2328c47ae1c16
ashokpandey7861 <94458715+ashokpandey7861@users.noreply.github.com>
2024-07-04 23:40:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,34 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import {
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
 
 import './css/style.css';
+
 import './charts/ChartjsConfig';
 
 // Import pages
 import Dashboard from './pages/Dashboard';
-import Dashboard1 from './pages/Dashboard1';
-
 
 function App() {
+
   const location = useLocation();
 
   useEffect(() => {
-    console.log('Current path:', location.pathname);
-    document.querySelector('html').style.scrollBehavior = 'auto';
-    window.scroll({ top: 0 });
-    document.querySelector('html').style.scrollBehavior = '';
-  }, [location.pathname]);
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+  }, [location.pathname]); // triggered on route change
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/dashboard.html" element={<Dashboard1 />} />
+        <Route exact path="/" element={<Dashboard />} />
       </Routes>
     </>
   );
 }
 
-export default function Root() {
-  return (
-    <Router>
-      <App />
-    </Router>
-  );
-}
+export default App;
